4b90103b530f51f6e337948a27edacf185f9ef41
Alert175 <40724267+Alert175@users.noreply.github.com>
2019-09-01 23:17:41 +0300

    Работа с маршрутизацией (routing)

--- polls/urls.py
+++ polls/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
-
+from django.urls import path, re_path
 from . import views
 
 
 urlpatterns = [
     path("", views.index),
-    path("user/", views.user)
+    # re_path(r"^user=(?P<user_name>\w+)", views.user_name, name="user_name"),
+    path("user=<str:user_name>", views.user_name),
+    re_path(r"^user=$", views.user_name),
+    re_path(r"^user$", views.user)
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
 
 
+# Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hello world!!!!!")
+    return HttpResponse("<h1>Hello world!!!!!</h1>")
 
 
 def user(request):
-    return HttpResponse("this is users page")
+    return HttpResponse("<h2>this is users page</h2>")
+
+
+def user_name(request, user_name="incognito"):
+    return HttpResponse("<h1>Hello {name}!</h1>".format(name=user_name))
 
