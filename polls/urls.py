e68be900548003cd37d8de4dc5770d8cebcb235d
Alert175 <40724267+alert175@users.noreply.github.com>
2019-09-01 23:24:10 +0300

    Работа с маршрутизацией (routing)
    Signed-off-by: Alert175 <40724267+Alert175@users.noreply.github.com>

--- polls/urls.py
+++ polls/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index),
-    # re_path(r"^user=(?P<user_name>\w+)", views.user_name, name="user_name"),
     path("user=<str:user_name>", views.user_name),
     re_path(r"^user=$", views.user_name),
     re_path(r"^user$", views.user)
